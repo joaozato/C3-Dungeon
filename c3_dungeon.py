64ffc362030035c396934888854de12e0a20c68a
Rafaela <rafaela.nnsgoulart@gmail.com>
2025-11-24 22:02:35 -0300

    mudanças na def(fase_jogo)

--- c3_dungeon.py
+++ c3_dungeon.py
@@ -19,15 +19,36 @@ def menu (win):
             background1.undraw()
             font.undraw()
             return "fase1"
-    
-
 
 def fase_jogo(win):
-    background2=Image(Point(540,400),"imgs/principal.png")
+    background2=Image(Point(540,400),"imgs/floresta.png")
     background2.draw(win)
-    caixa_dialogo=Rectangle(Point(260,580),Point(730,750))
-    caixa_dialogo.setFill("orange")
+    ponto_central = Point(140,675)
+    #retratoo
+    foto_retrato = Image(ponto_central, "imgs/principal_menor.png")
+    largura_retrato = foto_retrato.getWidth()
+    altura_retrato = foto_retrato.getHeight()
+
+    #calculo pra conseguir encaixar o sprite na caixa
+
+    x1 = 140 - (largura_retrato / 2) # centro - metade do tamanho
+    y1 = 675 - (altura_retrato / 2)
+    x2 = 140 + (largura_retrato / 2)
+    y2 = 675 + (altura_retrato / 2)
+
+    x1_dialogo = x2 + 20 #x2 onde o retrato termina + 20px de espaço
+    x2_dialogo = 1030 #x fixo
+
+
+    caixa_retrato = Rectangle(Point(x1,y1), Point(x2,y2))
+    caixa_retrato.draw(win)
+    caixa_retrato.setFill("black")
+    foto_retrato.draw(win)
+    caixa_dialogo=Rectangle(Point(x1_dialogo,y1),Point(x2_dialogo,y2))   
+    caixa_dialogo.setFill("black")
     caixa_dialogo.draw(win)
+
+
     teste='Você foi o escolhido para defender o C3\n Em um momento de fragilidade, quando os exames \n estão a beira de acontecer \n as criaturas da infernais querem as almas dos estudantes \n só você pode impedir isso Estudante!'
     teste_texto_atual=''
     Texto=Text(Point(500,650),'')
@@ -36,10 +57,6 @@ def fase_jogo(win):
         teste_texto_atual=teste_texto_atual+letra
         Texto.setText(teste_texto_atual)
         time.sleep(0.05)
-        
-    
-
-
 
 
 def main ():
@@ -50,3 +67,4 @@ def main ():
     win.getMouse()
     win.close()
 main()
+
